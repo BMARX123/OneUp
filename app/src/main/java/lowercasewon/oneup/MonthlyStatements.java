84aaa7198961bb19cdd3fd25b92d907b43d20abf
patches <mpatterm@purdue.edu>
2016-09-24 17:11:44 -0400

    implemented monthly statements

--- app/src/main/java/lowercasewon/oneup/MonthlyStatements.java
+++ app/src/main/java/lowercasewon/oneup/MonthlyStatements.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MonthlyStatements extends AppCompatActivity {
 
@@ -11,6 +12,34 @@ public class MonthlyStatements extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_statements);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Total Monthly Income: = " + FinancialOverview.totalMonthlyIncum);
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setText("Total Monthly Investments: = " + FinancialOverview.investmentPercentage * FinancialOverview.totalMonthlyIncum);
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        textView3.setText("Total Monthly Savings: = " + FinancialOverview.savingsPercentage * FinancialOverview.totalMonthlyIncum);
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+        textView4.setText("Total Monthly Spending: = " + FinancialOverview.moneySpentThisMonth);
+        if(!FinancialOverview.hasFourK) {
+            TextView textView5 = (TextView) findViewById(R.id.textView5);
+            textView5.setText("Monthly Deposit in IRA: = " + FinancialOverview.totalMonthlyIncum * FinancialOverview.rothIraPercent);
+
+        } else {
+            TextView textView6 = (TextView) findViewById(R.id.textView6);
+            textView6.setText("Monthly Deposit in 401K: = " + FinancialOverview.totalMonthlyIncum * FinancialOverview.rothIraPercent);
+
+        }
+        TextView textView7 = (TextView) findViewById(R.id.textView7);
+        textView7.setText("Monthly change in debt = " + -1*(FinancialOverview.totalMonthlyIncum - (FinancialOverview.totalMonthlyIncum*FinancialOverview.investmentPercentage) - (FinancialOverview.totalMonthlyIncum*FinancialOverview.savingsPercentage)-(FinancialOverview.totalMonthlyIncum*FinancialOverview.rothIraPercent)-(FinancialOverview.moneySpentThisMonth)));
+
+
+
+
+
+
+
+
+
     }
 
     @Override
