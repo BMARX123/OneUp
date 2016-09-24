5749bb721e2546727a69666ea05650c9ad4e4922
Brandon Marx <brandonmarx123@gmail.com>
2016-09-24 19:25:32 -0400

    Started Questionairre

---
+++ app/src/main/java/lowercasewon/oneup/Q2.java
@@ -0,0 +1,44 @@
+package lowercasewon.oneup;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Q2 extends AppCompatActivity {
+
+    EditText fName;
+    EditText lName;
+    EditText eMail;
+    EditText pWord;
+    Button button;
+    Context c = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q1);
+
+        fName = (EditText)findViewById(R.id.fName);
+        lName = (EditText)findViewById(R.id.lName);
+        eMail = (EditText)findViewById(R.id.email);
+        pWord = (EditText)findViewById(R.id.password);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                FinancialOverview.firstName = fName.toString();
+                FinancialOverview.lastName = fName.toString();
+                FinancialOverview.email = eMail.toString();
+                FinancialOverview.password = pWord.toString();
+
+                Intent i = new Intent(c, Q1.class);
+                startActivity(i);
+
+            }
+        });
+    }
+}
