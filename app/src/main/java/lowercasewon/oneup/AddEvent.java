4267f887d32073254a860f80ec88356b8e2d2a0b
patches <mpatterm@purdue.edu>
2016-09-25 01:20:51 -0400

    Fixed EditTexts in addEvent

--- app/src/main/java/lowercasewon/oneup/AddEvent.java
+++ app/src/main/java/lowercasewon/oneup/AddEvent.java
@@ -26,10 +26,10 @@ public class AddEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
-        billName = (EditText)findViewById(R.id.fName);
-        monthlyDue = (EditText)findViewById(R.id.lName);
-        intrestRate = (EditText)findViewById(R.id.email);
-        totalDue = (EditText)findViewById(R.id.password);
+        billName = (EditText)findViewById(R.id.editText);
+        monthlyDue = (EditText)findViewById(R.id.editText2);
+        intrestRate = (EditText)findViewById(R.id.editText3);
+        totalDue = (EditText)findViewById(R.id.editText4);
         button2 = (Button) findViewById(R.id.button2);
 
 
