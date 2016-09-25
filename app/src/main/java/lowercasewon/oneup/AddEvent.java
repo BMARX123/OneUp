72f93a0146121ea8a699ef02e759eeec4ef9a4b1
Brandon Marx <brandonmarx123@gmail.com>
2016-09-24 21:55:56 -0400

    CHANGE SOME STUFF FOR MATT

--- app/src/main/java/lowercasewon/oneup/AddEvent.java
+++ app/src/main/java/lowercasewon/oneup/AddEvent.java
@@ -10,7 +10,7 @@ public class AddEvent extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monthly_averages);
+        setContentView(R.layout.activity_add_event);
     }
 
     @Override
