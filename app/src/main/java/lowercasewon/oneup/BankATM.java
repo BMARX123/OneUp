5db9403f5fe7ff93b935fb77577f6a2229fe6c98
Brandon Marx <brandonmarx123@gmail.com>
2016-09-24 20:11:53 -0400

    Started Map Implementation

--- app/src/main/java/lowercasewon/oneup/BankATM.java
+++ app/src/main/java/lowercasewon/oneup/BankATM.java
@@ -1,139 +1,46 @@
 package lowercasewon.oneup;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.GpsSatellite;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.reimaginebanking.api.nessieandroidsdk.NessieError;
-import com.reimaginebanking.api.nessieandroidsdk.models.*;
-import com.reimaginebanking.api.nessieandroidsdk.NessieResultsListener;
-import com.reimaginebanking.api.nessieandroidsdk.models.PaginatedResponse;
-import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class BankATM extends FragmentActivity implements OnMapReadyCallback {
 
-public class BankATM extends AppCompatActivity {
-    public NessieClient client = NessieClient.getInstance("88d32ed949123a777cc5763009fbe502");
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client2;
-    Location location;
-    float latitude; // Latitude
-    float longitude; // Longitude
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_atm);
-
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_bank_atm, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
      */
-    public void getIndexApiAction() {
-        // If GPS enabled, get latitude/longitude using GPS Services
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            NessieResultsListener listener = new NessieResultsListener() {
-                @Override
-                public void onSuccess(Object result) {
-
-                }
-
-                @Override
-                public void onFailure(NessieError error) {
-
-                }
-            };
-            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            longitude = (float) location.getLongitude();
-            latitude = (float) location.getLatitude();
-            client.ATM.getATMs(latitude, longitude, (float) 1000, new NessieResultsListener() {
-                @Override
-                public void onSuccess(Object result) {
-                    PaginatedResponse<ATM> response = (PaginatedResponse<ATM>) result;
-                    System.out.println(response.getObjectList());
-                }
-
-                @Override
-                public void onFailure(NessieError error) {
-                    System.out.println("u fked up");
-                }
-            });
-        }
-
-    }
-
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client2.connect();
-        //AppIndex.AppIndexApi.start(client2, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //AppIndex.AppIndexApi.end(client2, getIndexApiAction());
-        client2.disconnect();
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- app/src/main/java/lowercasewon/oneup/Q2.java
+++ app/src/main/java/lowercasewon/oneup/Q2.java
@@ -2,6 +2,8 @@ package lowercasewon.oneup;
 
 import android.content.Context;
 import android.content.Intent;
+import android.renderscript.Double2;
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +12,10 @@ import android.widget.EditText;
 
 public class Q2 extends AppCompatActivity {
 
-    EditText fName;
-    EditText lName;
-    EditText eMail;
-    EditText pWord;
+    EditText age;
+    EditText mIncome;
+    EditText monthlySetAside;
+    EditText percentInvest;
     Button button;
     Context c = this;
 
@@ -22,21 +24,21 @@ public class Q2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q1);
 
-        fName = (EditText)findViewById(R.id.fName);
-        lName = (EditText)findViewById(R.id.lName);
-        eMail = (EditText)findViewById(R.id.email);
-        pWord = (EditText)findViewById(R.id.password);
+        age = (EditText)findViewById(R.id.age);
+        mIncome = (EditText)findViewById(R.id.income);
+        monthlySetAside = (EditText)findViewById(R.id.incomePercent);
+        percentInvest = (EditText)findViewById(R.id.investPercent);
         button = (Button)findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                FinancialOverview.firstName = fName.toString();
-                FinancialOverview.lastName = fName.toString();
-                FinancialOverview.email = eMail.toString();
-                FinancialOverview.password = pWord.toString();
+                FinancialOverview.age = Integer.parseInt(age.toString());
+                FinancialOverview.averageMonthlyIncome = Double.parseDouble(mIncome.toString());
+                FinancialOverview.savingsPercentage = Integer.parseInt(monthlySetAside.toString());
+                FinancialOverview.investmentPercentage = Integer.parseInt(percentInvest.toString());
 
-                Intent i = new Intent(c, Q1.class);
-                startActivity(i);
+                /*Intent i = new Intent(c, Q3.class);
+                startActivity(i);*/
 
             }
         });
